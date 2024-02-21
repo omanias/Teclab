fb14e7bd8e60cebaa380a3b26726c37f9c54c2bd
Omar Jesús Maniás <ojmanias1985@gmail.com>
2024-02-20 23:26:55 -0300

    first commit

---
+++ api2/api2.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+// Sentencia o declaración
+using namespace std;
+
+int main()
+{
+    // cout << "Hola Mundo" << endl;
+
+    // int salario = 1000;
+    // cout << salario;
+
+    string nombre = "Omar";
+
+    int edad = {30};
+
+    // Se declara la variable
+    double salario;
+
+    cout << "Introduce el salario: ";
+
+    // Se solicita la variable
+    cin >> salario;
+
+    cout << "Datos del empleado: " << endl;
+
+    cout << "Nombre: " << nombre << ". Edad: " << edad << ". Salario: " << salario;
+}
